a34b5ac3ba2eb3ce7f21309794d3a957f62e54dc
Tim <tim.weinert@gmail.com>
2020-01-23 15:44:41 +1100

    app complete - revise comments

--- app.js
+++ app.js
@@ -3,11 +3,17 @@
 const inputs = document.querySelectorAll('.controls input');
 
 
-function handleUpdate(){
-    console.log(this.value)
+function handleUpdate() {
+    //suffix variable = (data-sizing element)
+    //dataset will refer to any custom 'data-' elements created in the DOM (the .sizing specifies which one)
+    //The || " " = Or nothing. Remove to see what happens when function is init
+    const suffix = this.dataset.sizing || " ";
+    //DOM.DOM element.Change Style.se property.input element + name class + value class + suffix variable
+    //Select the name and value of the element and change sizing to the variable
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 //Add 'change' and 'mousemove' event listener functions to the inputs data
 //variable.prototype (parameter => function)
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
